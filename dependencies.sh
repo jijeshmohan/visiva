2f30e59de45dbc181fc56e22b0c166ad5a39b055
Jijesh Mohan <jijeshmohan@gmail.com>
2013-11-04 18:54:37 +0530

    added dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,1 +1,22 @@
-go get github.com/jessevdk/go-flags
+#!/bin/bash
+
+if [ -z "$GOPATH" ]; then
+    echo "Need to set GOPATH"
+    exit 1
+fi  
+ 
+goget(){
+	printf "Installing $1 : "
+	go get $1
+
+	if [ $? -eq 0 ]; then
+	    echo OK
+	else
+	    echo FAIL
+	    exit 1
+	fi
+}
+
+
+
+goget github.com/jessevdk/go-flags
